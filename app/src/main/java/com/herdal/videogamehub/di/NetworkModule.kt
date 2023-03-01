8a4219cd35742a0c2769bfa1b6bc555a63cfcc74
herdal06 <h.erdal721@gmail.com>
2023-03-02 00:35:40 +0300

    Create Network and Database modules

---
+++ app/src/main/java/com/herdal/videogamehub/di/NetworkModule.kt
@@ -0,0 +1,84 @@
+package com.herdal.videogamehub.di
+
+import androidx.viewbinding.BuildConfig
+import com.google.gson.Gson
+import com.herdal.videogamehub.data.remote.service.GameService
+import com.herdal.videogamehub.utils.constants.NetworkConstants.BASE_URL
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@[Module InstallIn(SingletonComponent::class)]
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggerInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        return httpLoggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideMoshiConverterFactory(): MoshiConverterFactory =
+        MoshiConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = Gson()
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory(): GsonConverterFactory =
+        GsonConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideBaseUrl(): String = BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory,
+        baseUrl: String
+    ): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(moshiConverterFactory)
+            .client(okHttpClient)
+            .baseUrl(baseUrl)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideGameService(retrofit: Retrofit): GameService =
+        retrofit.create(GameService::class.java)
+}
