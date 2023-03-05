501e74a53a84a02daa981ece78c7e2a0d193b4b8
herdal06 <h.erdal721@gmail.com>
2023-03-05 18:54:34 +0300

    Setup ViewPager2

--- app/src/main/java/com/herdal/videogamehub/presentation/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/herdal/videogamehub/presentation/favorite/FavoriteFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
+import com.herdal.videogamehub.R
 import com.herdal.videogamehub.databinding.FragmentFavoriteBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,6 +24,27 @@ class FavoriteFragment : Fragment() {
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewPager()
+    }
+
+    private fun setupViewPager() = binding.apply {
+        binding.viewPager.adapter =
+            FavoriteSlidePagerAdapter(fa = requireActivity())
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = getString(R.string.games)
+                }
+                1 -> {
+                    tab.text = getString(R.string.genres)
+                }
+            }
+        }.attach()
     }
 
     override fun onDestroyView() {
