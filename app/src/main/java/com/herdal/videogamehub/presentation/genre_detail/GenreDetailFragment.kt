23e032c18863eaf03343a59475aa5c6187e15f3e
herdal06 <h.erdal721@gmail.com>
2023-03-06 18:14:14 +0300

    Collect genre details

--- app/src/main/java/com/herdal/videogamehub/presentation/genre_detail/GenreDetailFragment.kt
+++ app/src/main/java/com/herdal/videogamehub/presentation/genre_detail/GenreDetailFragment.kt
@@ -5,7 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.herdal.videogamehub.common.Resource
 import com.herdal.videogamehub.databinding.FragmentGenreDetailBinding
+import com.herdal.videogamehub.domain.ui_model.GenreUiModel
+import com.herdal.videogamehub.utils.ext.collectLatestLifecycleFlow
+import com.herdal.videogamehub.utils.ext.hide
+import com.herdal.videogamehub.utils.ext.show
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +21,12 @@ class GenreDetailFragment : Fragment() {
     private var _binding: FragmentGenreDetailBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: GenreDetailViewModel by viewModels()
+
+    private val navigationArgs: GenreDetailFragmentArgs by navArgs()
+
+    private fun getGenreIdArgs() = navigationArgs.genreId
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,7 +34,33 @@ class GenreDetailFragment : Fragment() {
     ): View {
         _binding = FragmentGenreDetailBinding.inflate(inflater, container, false)
         val view = binding.root
+        collectGenreDetails(getGenreIdArgs())
         return view
+    }
+
+    private fun collectGenreDetails(genreId: Int) = binding.apply {
+        viewModel.getGenreDetails(genreId)
+        collectLatestLifecycleFlow(viewModel.genre) { resource ->
+            when (resource) {
+                is Resource.Loading -> {
+                    pbGenreDetails.show()
+                    tvGenreDetailError.hide()
+                }
+                is Resource.Success -> {
+                    pbGenreDetails.hide()
+                    tvGenreDetailError.hide()
+                    setupUI(resource.data)
+                }
+                is Resource.Error -> {
+                    pbGenreDetails.hide()
+                    tvGenreDetailError.show()
+                }
+            }
+        }
+    }
+
+    private fun setupUI(genre: GenreUiModel?) = binding.apply {
+        binding.genre = genre
     }
 
     override fun onDestroyView() {
