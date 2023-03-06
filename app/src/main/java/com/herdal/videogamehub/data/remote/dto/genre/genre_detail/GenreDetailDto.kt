8ccf8d17a4c4f31b73f1c710c249a92f4a06bb8b
herdal06 <h.erdal721@gmail.com>
2023-03-06 18:13:13 +0300

    Add genre uimodel mapper to genreDetailDto

--- app/src/main/java/com/herdal/videogamehub/data/remote/dto/genre/genre_detail/GenreDetailDto.kt
+++ app/src/main/java/com/herdal/videogamehub/data/remote/dto/genre/genre_detail/GenreDetailDto.kt
@@ -1,5 +1,6 @@
 package com.herdal.videogamehub.data.remote.dto.genre.genre_detail
 
+import com.herdal.videogamehub.domain.ui_model.GenreUiModel
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -18,3 +19,12 @@ data class GenreDetailDto(
     @Json(name = "slug")
     val slug: String
 )
+
+fun GenreDetailDto.toGenreUiModel() =
+    GenreUiModel(
+        id = id,
+        name = name,
+        slug = slug,
+        image_background = image_background,
+        description = description
+    )
